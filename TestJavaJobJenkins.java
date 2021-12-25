593e334efb8c4f18a871045f8b768c4bb5564e75
farooqabu <abufarooqsyed@gmail.com>
2021-12-25 15:25:19 -0600

    test commit

--- TestJavaJobJenkins.java
+++ TestJavaJobJenkins.java
@@ -3,7 +3,6 @@ package com.company;
 public class TestJavaJobJenkins {
 
     public static void main(String[] str) {
-        System.out.println("****************** Java Job Running  ******************");
-        System.out.println("****************** new Push event ***********************");
+        System.out.println("****************** GIT HUB RUNNING ******************");
     }
 }
