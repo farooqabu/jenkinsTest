e8c5c05d02902f7d7c960a5958f0af1f1d8ac3a2
farooqabu <abufarooqsyed@gmail.com>
2021-12-25 14:06:55 -0600

    test files added

---
+++ TestJavaJobJenkins.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TestJavaJobJenkins {
+
+    public static void main(String[] str) {
+        System.out.println("****************** Java Job Running Wah bhai wah ******************");
+    }
+}
