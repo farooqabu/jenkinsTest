a9eaaac2cee59128e1c7357a3f5faada112143ad
farooqabu <abufarooqsyed@gmail.com>
2021-12-25 15:24:16 -0600

    test commit

--- TestJavaJobJenkins.java
+++ TestJavaJobJenkins.java
@@ -5,6 +5,5 @@ public class TestJavaJobJenkins {
     public static void main(String[] str) {
         System.out.println("****************** Java Job Running  ******************");
         System.out.println("****************** new Push event ***********************");
-        System.out.println("****************** new Push event NEw***********************");
     }
 }
